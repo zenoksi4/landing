e49260c78b52cff35801880c3c3e164fd85e4417
zenoksi4 <zenoken4ik@gmail.com>
2022-07-11 20:55:27 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$('.wrapper a').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 370,   // по умолчанию «400»
+        easing: "linear" // по умолчанию «swing»
+    });
+
+    return false;
+});
